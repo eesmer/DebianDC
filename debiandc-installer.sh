50c00649d1a5d7482e222cddbe10357960734fc8
erkan <erkan.esmer@beyaz.net>
2020-12-05 18:07:18 +0300

    remove mainmenu. manager menu is single screen

--- debiandc-installer.sh
+++ debiandc-installer.sh
@@ -10,8 +10,16 @@ apt-get -y install openssh-server ntp
 apt-get -y install tmux vim
 apt-get -y install dnsutils net-tools
 apt-get -y install curl wget
-apt-get -y install ack expect krb5-user
+apt-get -y install expect krb5-user
 apt-get -y install htop
+
+# -----------------------------------------------------------------------------
+# SERVER CONFIG
+# -----------------------------------------------------------------------------
+sed -i "12a# DebianDC Config" /etc/ssh/sshd_config
+sed -i "13aPort 40022" /etc/ssh/sshd_config
+sed -i "14aPermitRootLogin yes" /etc/ssh/sshd_config
+systemctl restart ssh.service
 
 # -----------------------------------------------------------------------------
 # APPLICATION INSTALL & CONFIG
@@ -21,11 +29,7 @@ git clone https://github.com/eesmer/DebianDC.git
 cp -R DebianDC/debiandc/ /usr/local/ && cp DebianDC/README.md /usr/local/debiandc/ && rm -r DebianDC
 chown -R root:root /usr/local/debiandc
 chmod -R 744 /usr/local/debiandc
-chmod +x /usr/local/debiandc/mainmenu
 chmod +x /usr/local/debiandc/manager
-cp /usr/local/debiandc/mainmenu /usr/sbin/
 cp /usr/local/debiandc/manager /usr/sbin/
-chmod 755 /usr/sbin/mainmenu
 chmod 755 /usr/sbin/manager
-chmod +x /usr/sbin/mainmenu
 chmod +x /usr/sbin/manager

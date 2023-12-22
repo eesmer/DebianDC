37172b5d52ec8d82286e59768a8c9950374706b7
erkan.esmer <erkanesmer@gmail.com>
2023-12-22 18:52:53 +0300

    postinstall.sh updated

--- installer-files/debiandc-postinstall.sh
+++ installer-files/debiandc-postinstall.sh
@@ -34,24 +34,40 @@ update-alternatives --set editor /usr/bin/vim.basic
 # DebianDC-Setup Menu - settings of /etc/skel directory
 # -----------------------------------------------------------------------------
 mkdir -p /etc/skel/.local/share/applications
-cat > /etc/skel/.local/share/applications/DebianDC-Manager.desktop << EOF
+cat > /etc/skel/.local/share/applications/DebianDC-ADManager.desktop << EOF
 [Desktop Entry]
 Encoding=UTF-8
 Type=Application
 Name=DebianDC-Manager
 Terminal=false
-Comment=Active Directory Manager
+Comment=Active Directory ADManager
 Categories=IDE
 Icon=/usr/share/icons/gnome/48x48/actions/stock_up.png
 Exec=bash /usr/local/debiandc/manager
 EOF
+cat > /etc/skel/.local/share/applications/DebianDC-Utility.desktop << EOF
+[Desktop Entry]
+Encoding=UTF-8
+Type=Application
+Name=DebianDC-Utility
+Terminal=false
+Comment=Active Directory Environment Utility
+Categories=IDE
+Icon=/usr/share/icons/gnome/48x48/actions/stock_up.png
+Exec=bash /usr/local/debiandc/utility
+EOF
+
 mkdir -p /root/Desktop/DebianDC
-cp /etc/skel/.local/share/applications/DebianDC-Manager.desktop /root/Desktop/DebianDC/
-chmod +x /root/Desktop/DebianDC/DebianDC-Manager.desktop
+cp /etc/skel/.local/share/applications/DebianDC-ADManager.desktop /root/Desktop/DebianDC/
+cp /etc/skel/.local/share/applications/DebianDC-Utility.desktop /root/Desktop/DebianDC/
+chmod +x /root/Desktop/DebianDC/DebianDC-ADManager.desktop
+chmod +x /root/Desktop/DebianDC/DebianDC-Utility.desktop
 
 mkdir -p /root/.local/share/applications/
-cp /etc/skel/.local/share/applications/DebianDC-Manager.desktop /root/.local/share/applications/
-chmod +x /root/.local/share/applications/DebianDC-Manager.desktop
+cp /etc/skel/.local/share/applications/DebianDC-ADManager.desktop /root/.local/share/applications/
+cp /etc/skel/.local/share/applications/DebianDC-Utility.desktop /root/.local/share/applications/
+chmod +x /root/.local/share/applications/DebianDC-ADManager.desktop
+chmod +x /root/.local/share/applications/DebianDC-Utility.desktop
 
 echo "post-install finished"
 echo "Please reboot the machine"

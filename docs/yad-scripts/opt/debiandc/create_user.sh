b49a4acabf9effa8cab880cd6f4f6f3a585dc602
erkan.esmer <erkanesmer@gmail.com>
2025-08-30 23:53:38 +0300

    create_user.sh yad script edited

--- docs/yad-scripts/opt/debiandc/create_user.sh
+++ docs/yad-scripts/opt/debiandc/create_user.sh
@@ -14,3 +14,18 @@ source common-value
 #  exit 1
 #fi
 
+FIELDS=$("$YAD_BIN" $YAD_WIN \
+  --form \
+  --title="Create New User" \
+  --window-icon=system-users \
+  --text="<b>Enter New User Information</b>" \
+  --borders=10 \
+  --separator="|" \
+  --field="Username:ENTRY" \
+  --field="Name Surname:ENTRY" \
+  --field="Description(GECOS):ENTRY" \
+  --field="Password":HIDDEN \
+  --field="Password (Confirm)":HIDDEN \
+  --button="Cancel:1" \
+  --button="Create:0")
+

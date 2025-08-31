4786518d35890b60f5d31383574e1e105590e185
erkan.esmer <erkanesmer@gmail.com>
2025-08-31 11:26:03 +0300

    disable_user.sh yad script edited

--- docs/yad-scripts/opt/debiandc/disable_user.sh
+++ docs/yad-scripts/opt/debiandc/disable_user.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
-set -e
 set -o pipefail
 
 cd /opt/debiandc || exit 1
 source common-value
-
-#[ -f comman_value ] && source comman_value
-: "${YAD_BIN:=yad}"
-#: "${YAD_WIN:=--center --geometry=900x600}"
-
-#if ! command -v "$YAD_BIN" >/dev/null 2>&1; then
-#  echo "[ERR] 'yad' not installed" >&2
-#  exit 1
-#fi
-
-#!/bin/bash
-set -e
-set -o pipefail
-
-cd /opt/debiandc || exit 1
-source common-value
+LOG_FILE="/tmp/$(basename "$0" .sh).log"
 
 #[ -f comman_value ] && source comman_value
 : "${YAD_BIN:=yad}"
@@ -42,19 +26,22 @@ username=$( \
   --button="Cancel:1" --button="Disable:0" \
 )
 
-username="${username%%|}"
-
 status=$?
-[[ $status -ne 0 ]] && exit 0
+if [[ $status -eq 1 ]]; then
+  bash user_man.sh
+  exit 0
+fi
+
+username="${username%%|}"
 
 if [[ -z "$username" ]]; then
   "$YAD_BIN" $YAD_WIN --error --text="Username field cannot empty" --button=gtk-ok:0
-  exit 1
+  bash disable_user.sh
 fi
 
-{ samba-tool user disable "$username" 2>&1 | tee /tmp/disable_user.log ; } || true
+{ samba-tool user disable "$username" 2>&1 | tee $LOG_FILE ; } || true
 
-"$YAD_BIN" --text-info --title="Result" $YAD_WIN --button=gtk-ok:0 --filename=/tmp/disable_user.log
+"$YAD_BIN" --text-info --title="Result" $YAD_WIN --button=gtk-ok:0 --filename=$LOG_FILE
 
 bash user_man.sh
 

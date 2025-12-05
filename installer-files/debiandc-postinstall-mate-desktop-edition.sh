31e4ab91db22c3322c34e89a06d16827f1b0108a
erkan.esmer <erkanesmer@gmail.com>
2025-12-05 21:18:37 +0300

    postinstall-mate-desktop-edition.sh edited

--- installer-files/debiandc-postinstall-mate-desktop-edition.sh
+++ installer-files/debiandc-postinstall-mate-desktop-edition.sh
@@ -2,3 +2,12 @@
 
 # DebianDC - Post Install Script
 
+set -e
+export DEBIAN_FRONTEND=noninteractive
+ADDRESS="https://www.esmerkan.com/debiandc"
+
+echo "--------------------------------------------------"
+echo " DebianDC post-install is running (MATE Edition v2)"
+echo "--------------------------------------------------"
+sleep 2
+

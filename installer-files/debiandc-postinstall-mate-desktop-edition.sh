be62f58cdc967ec602620f7ea5790749f021070c
erkan.esmer <erkanesmer@gmail.com>
2025-12-05 21:20:07 +0300

    postinstall-mate-desktop-edition.sh edited

--- installer-files/debiandc-postinstall-mate-desktop-edition.sh
+++ installer-files/debiandc-postinstall-mate-desktop-edition.sh
@@ -11,3 +11,12 @@ echo " DebianDC post-install is running (MATE Edition v2)"
 echo "--------------------------------------------------"
 sleep 2
 
+export HOME="/root"
+
+# -----------------------------------------------------------------------------
+# Update/Upgrade
+# -----------------------------------------------------------------------------
+apt-get -y update
+apt-get -y upgrade
+apt-get -y autoremove
+

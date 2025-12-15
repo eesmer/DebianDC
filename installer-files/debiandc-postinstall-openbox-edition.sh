934360d25b33b2534a7a1c133fac8067f384a34f
erkan.esmer <erkanesmer@gmail.com>
2025-12-15 20:58:39 +0300

    openbox postinstall script edited for autostart config

--- installer-files/debiandc-postinstall-openbox-edition.sh
+++ installer-files/debiandc-postinstall-openbox-edition.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 apt-get install -y --no-install-recommends xorg openbox dbus-x11 x11-xserver-utils zenity yad
+
+cat > /root/.config/openbox/autostart << EOF
+exec >> "$HOME/.openbox-autostart.log" 2>&1
+echo "=== AUTOSTART $(date) ==="
+xsetroot -solid "#1e1e1e" || true
+EOF

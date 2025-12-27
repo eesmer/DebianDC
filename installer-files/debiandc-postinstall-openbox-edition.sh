019d8116ba4b888658386c32f531e755dbb9fc2a
erkan.esmer <erkanesmer@gmail.com>
2025-12-28 02:53:03 +0300

    slim login manager install line added

--- installer-files/debiandc-postinstall-openbox-edition.sh
+++ installer-files/debiandc-postinstall-openbox-edition.sh
@@ -11,10 +11,11 @@ apt-get -y install ansible
 apt-get -y install syslog-ng #for auth.log
 apt-get -y install xrdp
 apt-get -y install --no-install-recommends xorg openbox dbus-x11 x11-xserver-utils xterm zenity yad
-apt-get -y install --no-install-recommends lightdm lightdm-gtk-greeter
+apt-get -y install slim
+#apt-get -y install --no-install-recommends lightdm lightdm-gtk-greeter
 apt-get -y install netsurf-gtk xdg-utils
 apt-get -y install --no-install-recommends policykit-1 lxqt-policykit
-systemctl enable lightdm
+#systemctl enable lightdm
 
 # -----------------------------------------------------------------------------
 # APPLICATION INSTALL & CONFIG

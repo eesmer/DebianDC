adf6ae532c7e9ab72271f550c6d685410bcbdf95
erkan.esmer <erkanesmer@gmail.com>
2026-01-03 09:36:55 +0300

    openbox installer updated

--- installer-files/debiandc-postinstall-openbox-edition.sh
+++ installer-files/debiandc-postinstall-openbox-edition.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
 apt-get -y install git
-apt-get -y install zenity
-apt-get -y install openssh-server chrony ntpdate # - ntp (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1033088)
+apt-get -y install openssh-server chrony
 apt-get -y install dnsutils net-tools
 apt-get -y install curl wget
-apt-get -y install ack expect krb5-user krb5-config
-apt-get -y install tmux vim htop
-apt-get -y install ansible
+apt-get -y install ack expect #krb5-user krb5-config
 apt-get -y install syslog-ng #for auth.log
-apt-get -y install xrdp
-apt-get -y install --no-install-recommends xorg openbox dbus-x11 x11-xserver-utils xterm zenity yad
-apt-get -y install slim
-#apt-get -y install --no-install-recommends lightdm lightdm-gtk-greeter
-apt-get -y install netsurf-gtk xdg-utils
-apt-get -y install --no-install-recommends policykit-1 lxqt-policykit
-#systemctl enable lightdm
+apt-get -y install openbox
+apt-get -y install tmux vim htop
+apt-get -y install xdg-utils xorg dbus-x11 x11-xserver-utils xserver-xorg-input-libinput
+apt-get -y install slim netsurf-gtk zenity xterm xrdp
+#apt-get -y install ansible
 
 # -----------------------------------------------------------------------------
 # APPLICATION INSTALL & CONFIG
 # -----------------------------------------------------------------------------
 git clone https://github.com/eesmer/DebianDC.git
-cp -R DebianDC/debiandc/ /usr/local/ && cp DebianDC/README.md /usr/local/debiandc/ && rm -r DebianDC
+cp -R DebianDC/debiandc/ /usr/local/ && cp DebianDC/README.md /usr/local/debiandc/
 chown -R root:root /usr/local/debiandc
 chmod -R 744 /usr/local/debiandc
 chmod +x /usr/local/debiandc/manager
 #cp /usr/local/debiandc/manager /usr/sbin/
 #chmod 755 /usr/sbin/manager
 #chmod +x /usr/sbin/manager
+
+cp -r /usr/share/slim/themes/desktop-slim-theme /usr/share/slim/themes/debiandc
+cp DebianDC/Documentation/assets/branding/DebianDC-LoginScreenImage.png /usr/share/slim/themes/debiandc/background.jpg && chmod 644 /usr/share/slim/themes/debiandc/background.jpg
+rm -rf DebianDC
+
+cat > /etc/slim.conf << EOF
+# DebianDC - SLiM Login Manager Configuration
+# This file is managed by DebianDC installer.
+# Manual changes may be overwritten.
+current_theme       debiandc
+login_cmd           exec openbox-session
+sessions            openbox
+allowed_users       root
+fail_delay          3
+welcome_msg         DebianDC - Authorized Access Only (%host %domain)
+shutdown_msg        DebianDC - The system is halting...
+reboot_msg          DebianDC - The system is rebooting...
+lockfile            /run/slim.pid
+logfile             /var/log/slim.log
+EOF
+chmod 644 /etc/slim.conf
 
 mkdir -p /root/.config/openbox
 cp /etc/xdg/openbox/{rc.xml,autostart} /root/.config/openbox/
@@ -41,9 +57,9 @@ if command -v dbus-launch >/dev/null 2>&1; then
   eval "$(dbus-launch --sh-syntax --exit-with-session)"
 fi
 sleep 1
-#/usr/local/bin/debiandc-console.sh &
 /usr/local/debiandc/manager &
 EOF
+chmod +x /root/.config/openbox/autostart 
 
 cat > /root/.config/openbox/menu.xml << EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -54,7 +70,7 @@ cat > /root/.config/openbox/menu.xml << EOF
         <command>/usr/local/debiandc/manager</command>
         </action>
         </item>
-	<item label="Restart DebianDC Manager Screen">
+	<item label="Restart DebianDC Manager">
         <action name="Execute">
         <command>/usr/local/bin/debiandc-console-restart.sh</command>
         </action>
